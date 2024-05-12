53b782ea541fe8b0cf48650bced5093b13548c46
Akash Morwal <amorwal5@gmail.com>
2024-05-12 23:36:04 +0530

    new program of type casting is successfully done (12-05-2024)

---
+++ type casting/Typecastingex1.java
@@ -0,0 +1,18 @@
+class Typecastingex1
+{
+
+	public static void main(String[] args) {
+		
+		byte a = 1;
+		short b = a;
+		int c = a;
+		long d = a;
+		float e = a;
+		double f = a;
+		//char g = a;// CTC for PLC
+		//in type casting we write as
+		char g = (char)a;   //type casting
+
+		System.out.println(a);
+	}
+}
