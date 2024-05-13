7c443030ab7920fff35a5aeb79bd5218614653ed
Akash Morwal <akashbhau101@gmail.com>
2024-05-13 23:07:41 +0530

    new program of method overloading is updated (13-05-2024)

---
+++ polymorphisum/MethodOverloading.java
@@ -0,0 +1,19 @@
+///method overloading example in polymorphisum
+
+class MethodOverloading
+
+{
+	public static void main(String[] args) {
+		
+		System.out.println("BYEE");
+	}
+	public static void  main()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(int a){
+		System.out.println("Hieee");
+	}
+
+}
