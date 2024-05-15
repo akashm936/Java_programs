850afad35d2ab3974cc3099fbecacfdeccf9fd4c
Akash Morwal <akashbhau101@gmail.com>
2024-05-15 23:56:31 +0530

    new program is updated (15-05-2024)

---
+++ upcasting & dawncasting/DemoEx3.java
@@ -0,0 +1,52 @@
+//This Program is used for upcasting and dawnCasting 
+
+class Parent
+{
+	void greeting()
+	{
+		System.out.println("Good Morning");
+	}
+	void m1()
+	{
+		System.out.println("Hello From");
+	}
+}
+
+class Child extends Parent
+{
+	void greeting()
+	{
+		System.out.println("Good Morning");
+	}
+	void M2()
+	{
+		System.out.println("HIE From the m2()");
+	}
+
+}
+
+class Child2 extends Parent
+{
+	void greeting()
+	{
+		System.out.println("Good Evening");
+	}
+	void m3()
+	{
+		System.out.println("Hello Form m3 Method");
+	}
+}
+
+class DemoEx1
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		Child obj = new Child();
+		obj.greeting();
+		Parent obj1 = new Child(); // this is a UpCasting
+
+		Child obj2 = (Child)obj1;  // this is the dawnCasting Method
+		      obj2 = m2();
+	}
+}
