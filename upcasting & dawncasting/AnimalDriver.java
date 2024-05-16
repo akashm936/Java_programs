9a52c63e270215349a84c6aafd40fdeef5d0a20f
Akash Morwal <akashbhau101@gmail.com>
2024-05-16 12:11:48 +0530

    new program is updated of the instanceof (16-05-2024)

--- upcasting & dawncasting/AnimalDriver.java
+++ upcasting & dawncasting/AnimalDriver.java
@@ -1,1 +1,21 @@
-class
+class Animal
+{
+
+}
+
+class Dog1 extends Animal
+{
+	void m1(){
+		System.out.println("Dog 1 from ");
+	}
+}
+
+class AnimalDriver{
+
+	public static void main(String[] args) {
+		
+		Dog1 d = new Dog1();
+		System.out.println(d instanceof Animal);
+		d.m1();
+	}
+}
