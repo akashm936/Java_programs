39b16dc8baa3680d1266dc946b931ac981ec2ffb
Akash Morwal <akashbhau101@gmail.com>
2024-05-18 12:22:47 +0530

    new java Program of CharToInt is successfully Run and test cases execute ( 18-05-2024)

--- tech M assignment/CharTOInt.java
+++ tech M assignment/CharTOInt.java
@@ -6,21 +6,40 @@ class CharTOInt
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter Alphabet : ");
-		char al = sc.next().charAt(0);
-        int num = 1;
         
 
-		for(char i='A';i<='Z';i++)
+        //logic for A = 1
+		// System.out.print("Enter Alphabet : ");
+		// char al = sc.next().charAt(0);
+        // int num = 1;
+        
+
+		// for(char i='A';i<='Z';i++)
+		// {
+		// 	// count++;
+		// 	int op=num++;
+   		//     if(al==i)
+		//     {
+		//     	System.out.println(al + " = " + op);
+		//     }
+		// }
+
+		System.out.print("Enter a Character : ");
+		String str = sc.next().toUpperCase();
+		int op = 0;
+
+		for(int i=0;i<str.length();i++)
 		{
-			// count++;
-			int op=num++;
-   		    if(al==i)
-		    {
-		    	System.out.println(al + " = " + op);
-		    }
+			char ch = str.charAt(i);
+			if(i==0)
+		        op = (ch-64);
+		    else
+			   op = (op*26)+(ch-64);
+			
+			System.out.println(str + " = " + op);
 		}
 
 	}
 }
+
+//
