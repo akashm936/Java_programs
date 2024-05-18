df3f29746106b7285f3cb73bb8d09822bf81dab4
Akash Morwal <akashbhau101@gmail.com>
2024-05-18 11:56:16 +0530

    new program of abstraction is successfully Run (18-05-2024)

--- Abstraction/ArithDriver.java
+++ Abstraction/ArithDriver.java
@@ -1,4 +1,4 @@
-class Arithmatic
+abstract class Arithmatic
 {
 	abstract int add(int num1,int num2);
 	abstract int sub(int num1,int num2);
@@ -30,9 +30,9 @@ class ArithDriver
 {
 	public static void main(String[] args) {
 		Operation obj = new Operation();
-		System.out.println(obj.add(10,20));
-		System.out.println(obj.sub(10,20));
-		System.out.println(obj.mul(10,20));
-		System.out.println(obj.div(10,20));
+		System.out.println("the Addition of Two number (10+20) : "+obj.add(10,20));
+		System.out.println("the Subtraction of Two number (10-20) : "+obj.sub(10,20));
+		System.out.println("the Multiplication of Two number (10*20) : "+obj.mul(10,20));
+		System.out.println("the Division of Two number (10/20) : "+obj.div(10,20));
 	}
 }
