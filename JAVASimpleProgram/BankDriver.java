1b04127aa7c2983196248f239539234ee4e654c2
Akash Morwal <akashbhau101@gmail.com>
2024-05-19 12:27:18 +0530

    new program is successfully run (19-05-2024)

--- JAVASimpleProgram/BankDriver.java
+++ JAVASimpleProgram/BankDriver.java
@@ -2,44 +2,63 @@ class Bank
 {
     private String name;
     String branch;
-    long acc_no;
-    String ifsc_code;
+    //long acc_no;
+    //String ifsc_code;
     private String password;
 
-    Bank(String name, String branch,long branch,String ifsc_code,String password)
+    Bank(String name,String branch, String password)
     {
     	this.name=name;
     	this.branch = branch;
-    	this.acc_no=acc_no;
-    	this.ifsc_code=ifsc_code;
+    	//this.acc_no=acc_no;
+    	//this.ifsc_code=ifsc_code;
     	this.password=password;
     }	
       
-    public String getname(String newname)
+    public String getname()
     {
-    	System.out.println("Bank Name : " + newname);
-    	return newname();
+    	//System.out.println("Bank Name : " + newname);
+    	return name;
     }
 
-    public String setpassword(String newpassword)
+    public void setname(String newname)
     {
     	name = newname;
     }
 
-    public String getpassword(String newpassword){
-    	return newpassword();
+    public String getpassword(){
+    	return password;
     }
 
-    public String setpassword(String newpassword)
+    public void setpassword(String newpassword)
     {
     	password = newpassword;
     }
 
+    public void displaybank(){
+
+    	System.out.println(name);
+    	System.out.println(branch);
+    	//System.out.println(acc_no);
+    	//System.out.println(ifsc_code);
+    	System.out.println(password);
+
+    	//return name;
+    }
+
+
 
 }
-class BanakDriver
+
+class BankDriver
 {
-     Bank obj = new Bank("Kotak Bank","Nagpur",89128372637l,"NAG62323","AKM@123");
-     obj.getname(obj.newname);
+     public static void main(String[] args) {
+     	
+     	Bank obj = new Bank("Kotak Bank","Nagpur","AKM@123");
+        System.out.println("Bank name is : "+obj.getname());
+        System.out.println("user password : "+obj.getpassword());
+        System.out.println("Branch City : "+ obj.branch);
+
+     }
 
 }
