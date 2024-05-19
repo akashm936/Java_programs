e719ba48818c3935704f54f3b9a8988de247cf40
Akash Morwal <akashbhau101@gmail.com>
2024-05-19 12:29:26 +0530

    new program is updated (19-05-2024)

--- JAVASimpleProgram/BankDriver.java
+++ JAVASimpleProgram/BankDriver.java
@@ -2,16 +2,16 @@ class Bank
 {
     private String name;
     String branch;
-    //long acc_no;
-    //String ifsc_code;
+    long acc_no;
+    String ifsc_code;
     private String password;
 
-    Bank(String name,String branch, String password)
+    Bank(String name,String branch,long acc_no,String ifsc_code, String password)
     {
     	this.name=name;
     	this.branch = branch;
-    	//this.acc_no=acc_no;
-    	//this.ifsc_code=ifsc_code;
+    	this.acc_no=acc_no;
+    	this.ifsc_code=ifsc_code;
     	this.password=password;
     }	
       
@@ -39,22 +39,19 @@ class Bank
 
     	System.out.println(name);
     	System.out.println(branch);
-    	//System.out.println(acc_no);
-    	//System.out.println(ifsc_code);
+    	System.out.println(acc_no);
+    	System.out.println(ifsc_code);
     	System.out.println(password);
 
     	//return name;
     }
-
-
-
 }
 
 class BankDriver
 {
      public static void main(String[] args) {
      	
-     	Bank obj = new Bank("Kotak Bank","Nagpur","AKM@123");
+     	Bank obj = new Bank("Kotak Bank","Nagpur",3624633924693l,"FFN736473","AKM@123");
         System.out.println("Bank name is : "+obj.getname());
         System.out.println("user password : "+obj.getpassword());
         System.out.println("Branch City : "+ obj.branch);
