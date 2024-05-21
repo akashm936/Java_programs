bff92065789c69c448c0aa78a5879af8ac2a2b03
Akash Morwal <akashbhau101@gmail.com>
2024-05-21 21:44:01 +0530

    new program is updated (21-05-2024)

---
+++ JAVASimpleProgram/AbstractionExample1.java
@@ -0,0 +1,25 @@
+class Student
+{
+	String str1 = "Hello Akash";
+	Parent()
+	{
+		super();
+	}
+	void Akash(){
+		System.out.println("Hello I Am Akash");
+	}
+	void Gaurav(){
+		System.out.println("Hello I am Gaurav");
+	}
+}
+
+
+class Qspider extends Student
+{
+	String str1 = "Hello Qspider";
+	Qspider()
+	{
+		super();
+		System.out.println("This is Qspider Constructor");
+	}
+}
