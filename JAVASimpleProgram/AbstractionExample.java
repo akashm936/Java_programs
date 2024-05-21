3ff613724574e8cd5ce178c6e3c4939e8201d5a4
Akash Morwal <akashbhau101@gmail.com>
2024-05-21 21:29:46 +0530

    new program is starting (21-05-2024)

--- JAVASimpleProgram/AbstractionExample.java
+++ JAVASimpleProgram/AbstractionExample.java
@@ -1,4 +1,13 @@
 class Parent
 {
 	String str = "Hello Parent";
+	Parent()
+	{
+		super();
+		System.out.println("Hello From Parent Constructor");
+	}
+	void m1()
+	{
+		System.out.println("m1() Parent");
+	}
 }
