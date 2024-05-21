6005b29b12ede33320c0a3531f140bbe4cd9249d
Akash Morwal <akashbhau101@gmail.com>
2024-05-21 21:34:58 +0530

    new program is successfully run (21-05-2024)

--- JAVASimpleProgram/AbstractionExample.java
+++ JAVASimpleProgram/AbstractionExample.java
@@ -10,4 +10,37 @@ class Parent
 	{
 		System.out.println("m1() Parent");
 	}
+	void m2()
+	{
+		System.out.println("m2() Parent");
+	}
+}
+
+class Child extends Parent
+{
+	String str = "Hello Child";
+	Child()
+	{
+		super();
+		System.out.println("Hello Form Child Constructor");
+	}
+
+	void m3()
+	{
+		System.out.println(str);
+		System.out.println(super.str);
+		System.out.println("m1");
+		super.m1();
+		super.m2();
+
+	}
+}
+
+class AbstractionExample
+{
+	public static void main(String[] args) {
+		
+		Child obj = new Child();
+		obj.m3();
+	}
 }
