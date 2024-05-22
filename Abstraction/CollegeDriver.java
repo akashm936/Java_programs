ceda25b7721a1c16f8060bb8abcd7fa90ad5054f
Akash Morwal <akashbhau101@gmail.com>
2024-05-22 12:30:14 +0530

    new program is updated (22-05-2024)

--- Abstraction/CollegeDriver.java
+++ Abstraction/CollegeDriver.java
@@ -43,6 +43,17 @@ class Student extends Collge
 		this.con_no = con_no;
 	}
 
+	Student(String collge_Name, String address, String collge_Type, String principle_name,int roll_no, String student_Name,String stud_address,long con_no)
+	{
+		super(collge_Name,address,collge_Type,principle_name,roll_no,student_Name,stud_address,con_no);
+		this.roll_no = roll_no;
+		this.student_Name = student_Name;
+		this.stud_address = stud_address;
+		this.con_no = con_no;
+
+		System.out.println(" Student Constructor is overload");
+	}
+
 	void displayStudent(){
 
 		System.out.println("******** Student Details ************");
