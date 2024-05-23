d8ec8b227242b09ebe8de9994f0322a3eb8f2697
Akash Morwal <akashbhau101@gmail.com>
2024-05-23 14:06:07 +0530

    new program is updated of type  casting (23-05-2024)

---
+++ Pattern Program/Type_conversion.java
@@ -0,0 +1,27 @@
+class Type_conversion
+{
+	public static void main(String[] args) {
+		
+		char a=10;
+
+		byte x=a;
+		short b=a;
+		int c=a;
+		long d=a;
+		float e=a;
+		double f=a;
+
+		char g=a;
+		System.out.println(a);
+   
+      //type casting rule  of wilding
+ 	     //byte =   all except char
+		 //int    = all except  ,byte, char, short
+		 //short = byte except  char
+		 //long = int except  short,byte,char 
+         //float =  except double 
+         //double = not convert with any type 
+         //char = all except byte short
+        
+	}
+}
