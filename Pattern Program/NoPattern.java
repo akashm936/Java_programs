2b7593989ae391b9bdd21e591d7f0f2210ee5d0a
Akash Morwal <akashbhau101@gmail.com>
2024-05-24 08:46:44 +0530

    new pattern program updated (24-05-2024)

--- Pattern Program/NoPattern.java
+++ Pattern Program/NoPattern.java
@@ -1,4 +1,20 @@
 class NoPattern
 {
+
+	public static void main(String[] args) {
+		
+		int n=5;
+		int x=2;
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				System.out.println("%2d",x);
+				x+=2;
+			}
+			System.out.println();
+		}
+	}
 	
 }
