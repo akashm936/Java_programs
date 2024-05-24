842268f771fab569be4157e1b7a935426e270b05
Akash Morwal <akashbhau101@gmail.com>
2024-05-24 12:41:40 +0530

    new program is updated (24-05-2024)

---
+++ Pattern Program/MultipleInheritanceDriver.java
@@ -0,0 +1,34 @@
+interface Parent1{
+	
+	default void greeting(){
+	    System.out.println("Good Morning From Parent1");    
+	}
+}
+
+interface Parent2{
+     default void greeting(){
+     	System.out.println("Good Morning form Parent2");
+     }
+}
+
+interface Child extends Parent1,Parent2
+{
+   default void greeting()
+   	{
+   		System.out.println("Good Morning From Child");
+   		Parent1.super.greeting();
+   		Parent2.super.greeting();
+   	}
+}
+
+class Imp implements Child{
+}
+
+class MultipleInheritanceDriver{
+
+	public static void main(String[] args) {
+		
+		Imp obj = new Imp();
+		obj.greeting(); 
+	}
+}
