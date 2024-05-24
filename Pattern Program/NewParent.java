16487cebefcd6d9ac41004dea9a9ce264434cea1
Akash Morwal <akashbhau101@gmail.com>
2024-05-24 11:44:37 +0530

    new program is updated (24-05-2024)

---
+++ Pattern Program/NewParent.java
@@ -0,0 +1,22 @@
+abstract class Parent1
+{
+	
+	abstract void greeting();
+}
+class Parent2 extends Parent1
+{
+   void greeting()
+   {
+   	System.out.println("Good Afternoon");
+   }
+}
+
+class NewParent{
+
+	public static void main(String[] args) {
+		
+		Parent2 obj = new Parent2();
+		obj.greeting();
+	}
+
+}
