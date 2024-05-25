02fb367862f9f334708c8bdc2cea9d4a7eb09730
Akash Morwal <akashbhau101@gmail.com>
2024-05-25 10:29:53 +0530

    new program is updated (25-05-2024)

---
+++ Pattern Program/EmployeeDriver.java
@@ -0,0 +1,32 @@
+class Employee
+{
+    String empid;
+	String name;
+	long con;
+	String add;
+	
+	Employee(String empid,String name,long con, String add)
+	{
+		this.empid= empid;
+		this.name = name;
+		this.con= con;
+		this.add = add;
+	}
+	@Override
+	public void finalize(){
+		System.out.println("object Destroid successfully");
+	}
+}
+
+class EmpolyeeDriver
+{
+	public static void main(String[] args) {
+		
+		Employee obj = new Employee("E001","Akash",767346374l,"Jalna");
+		System.out.println(obj);
+		obj.finalize();
+		System.out.println(obj.name);
+
+
+	}
+}
