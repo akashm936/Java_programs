6a32fe8f5c3da3ed4d2c89a239500a731592f83b
Akash Morwal <akashbhau101@gmail.com>
2024-05-25 14:42:19 +0530

    new program is updated (25-05-2024)

--- Pattern Program/GarbageCollection.java
+++ Pattern Program/GarbageCollection.java
@@ -1,3 +1,5 @@
+// run status : true
+
 import java.util.*;
 
 class MobileBox
---
+++ Pattern Program/Pattern17.java
@@ -0,0 +1,18 @@
+// run status  : true
+
+class Program17
+{
+	public static void main(String[] args) 
+	{
+	  int n = 5;
+
+	  for(int i=1;i<=n;i++)
+	  {
+	  	for(int j=1;j<=n;j++)
+	  	{
+	  		System.out.print("%2d",2*(i+j)-3);
+	  	}
+	  	System.out.println();
+	  } 
+	}
+}
