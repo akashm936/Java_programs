1ba5ef297ce70ce30da07479924898dbcba4f28f
Akash Morwal <akashbhau101@gmail.com>
2024-05-26 11:29:35 +0530

    new program swapNumber is updated  (26-05-2024)

---
+++ Pattern Program/SwapNumber.java
@@ -0,0 +1,18 @@
+class SwapNumber
+{
+	public static void main(String[] args) {
+		
+		int a = 15;
+		int b = 30;
+		int c;
+
+        c=a;
+        a=b;
+        b=c;
+		System.out.println("a = " + a);
+		System.out.println("b = "+ b);
+		//System.out.println(c);
+
+
+	}
+}
