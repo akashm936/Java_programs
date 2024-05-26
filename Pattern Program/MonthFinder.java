667a21e52923f2706fd4abf58b5c873baf8f3ffe
Akash Morwal <akashbhau101@gmail.com>
2024-05-26 11:58:25 +0530

    new program is updated (26-05-2024)

--- Pattern Program/MonthFinder.java
+++ Pattern Program/MonthFinder.java
@@ -8,56 +8,43 @@ class MonthFinder
    System.out.println("Enter the number :");   
    int month = sc.nextInt();
 
-   if(month=1)
-   {
+   if(month==1){
    	System.out.println(month + "= Janewary"  );
    }
-    else if(month=2)
-   {
+   else if(month==2){
    	System.out.println(month + "= Febwary"  );
    }
-    else if(month=3)
-   {
+   else if(month==3){
    	System.out.println(month + "= March"  );
    }
-    else if(month=4)
-   {
+   else if(month==4){
    	System.out.println(month + "= April"  );
    }
-    else if(month=5)
-   {
+   else if(month==5){
    	System.out.println(month + "= May"  );
    }
-    else if(month=6)
-   {
+   else if(month==6){
    	System.out.println(month + "= June"  );
    }
-    else if(month=7)
-   {
+   else if(month==7){
    	System.out.println(month + "= July"  );
    }
-    else if(month=8)
-   {
+   else if(month==8){
    	System.out.println(month + "= Agust"  );
    }
-    else if(month=9)
-   {
+   else if(month==9){
    	System.out.println(month + "= September"  );
    }
-    else if(month=10)
-   {
+   else if(month==10){
    	System.out.println(month + "= Octomber"  );
    }
-    else if(month=11)
-   {
+   else if(month==11){
    	System.out.println(month + "= Nuhmber"  );
    }
-    else if(month=12)
-   {
+    else if(month==12){
    	System.out.println(month + "= December"  );
    }
-   else
-   {
+   else{
    	System.out.println("Invalid Number" );
    }
 }
