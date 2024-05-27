36e8aa0dabb5c253bdcaabee3751f770c7e94f39
Akash Morwal <akashbhau101@gmail.com>
2024-05-27 15:00:57 +0530

    new program is updated (27-05-2024)

---
+++ object class/HumanDriver.java
@@ -0,0 +1,45 @@
+class Human implements Cloneable{
+	
+	String name;
+	String place;
+	String gender;
+	String proffesion;
+
+	Human(String name, String place,String gender,String proffesion)
+	{
+		this.name =name;
+		this.place = place;
+		this.gender = gender;
+		this.proffesion = proffesion;
+	}
+	
+	public String toString()
+	{
+		return this.name +" "+ this.place+" " + this.gender+" " + this.proffesion;
+	}
+
+	public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+
+}
+
+class HumanDriver{
+
+	public static void main(String[] args) throws CloneNotSupportedException 
+	{
+		
+		Human obj1 = new Human("Akash","Jalna","Male","SDE");
+		System.out.println(obj1);
+
+		Human obj2 = (Human)obj1.clone();
+		System.out.println(obj2);
+
+		obj2.name = "Gaurav";
+		obj2.place ="Chh. sambhajinagar";
+		System.out.println(obj1);
+		System.out.println(obj2);
+	}
+}
+//name ,place , gender proffesion
---
+++ object class/ObjectDriver.java
@@ -0,0 +1,39 @@
+class Student
+{
+	String sName;
+	String place;
+	String branch;
+	int yop;
+
+	Student(String sName,String place,String branch,int yop)
+	{
+		this.sName = sName;
+		this.place =place;
+		this.branch = branch;
+		this.yop = yop;
+	} 
+
+	public boolean equals(Object obj)
+	{
+		Student obj2 = (Student)obj;
+		if((this.sName==obj2.sName)&&(this.place==obj2.place)&&(this.branch==obj2.branch)&&(this.yop==obj2.yop))
+		{
+			return true;
+		}
+		else {
+			//System.out.println(this.sName + " is not match with " + obj2.sName);
+			return false;
+		}
+	}
+}
+class ObjectDriver
+{
+	public static void main(String[] args) {
+		
+
+		Student obj1 = new Student("Akash","Jalna","CSE",2024);
+		Student obj2 = new Student("Gaurav","Chh. Sambhajinagar","CSE",2024);
+		System.out.println(obj1.equals(obj2));
+	}
+
+}
