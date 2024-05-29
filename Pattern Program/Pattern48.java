00f18889b4fb4f0f831f78fbda7e0141cec62947
Akash Morwal <akashbhau101@gmail.com>
2024-05-29 08:20:05 +0530

    new program of Pattern is updated (29-05-2024)

--- Pattern Program/Pattern48.java
+++ Pattern Program/Pattern48.java
@@ -1,6 +1,27 @@
 class Pattern48
 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		int n=5;
+
+		int x = 1;
+		int y;
+		int t;
+
+
+		for(int i = n;i>=1;i--)
+		{
+			y=i;
+			t=x;
+
+			for(int j=n;j>=i;j--)
+			{
+				System.out.printf("%2d",t);
+				t=t-y;
+				y++;
+			}
+			x = x+i;
+			System.out.println();
+		}
 	}
 }
