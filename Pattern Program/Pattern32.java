0cfbf53291ee503f40663a2e6ca27c54f44e6ec4
Akash Morwal <akashbhau101@gmail.com>
2024-05-30 09:44:00 +0530

    new program is updated  (30-05-2024)

--- Pattern Program/Pattern32.java
+++ Pattern Program/Pattern32.java
@@ -2,6 +2,17 @@ class Pattern32
 {
 	public static void main(String[] args) {
 		
-		int n = 5
+		int n = 5;
+		int x ;
+		for(int i=0;i<n;i++)
+		{
+			x=i;
+			for(int j=1;j<=n;j++)
+			{
+				x+=n;
+				System.out.print((char)(x-n+65)+" ");
+			}
+			System.out.println();
+		}
 	}
 }
