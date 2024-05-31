c0171dc682a21d87180cc2ebea4159e4131ba113
Akash Morwal <akashbhau101@gmail.com>
2024-05-31 07:59:18 +0530

    new program is successfully run (31-05-2024)

--- Pattern Program/WebAppDriver.java
+++ Pattern Program/WebAppDriver.java
@@ -3,7 +3,32 @@ class WebApp
 	String name;
 	String technology;
 	String database;
-	String noofDevelopers;
+	int noofDevelopers;
 
-	WebApp(String name,String technology, String database, String noofDevelopers)
+	WebApp(String name,String technology, String database, int noofDevelopers)
+	{
+		this.name = name;
+		this.technology = technology;
+		this.database = database;
+		this.noofDevelopers = noofDevelopers;
+	}
+
+	public void displayWebAppInfo()
+	{
+		System.out.println("Information about Web Application");
+		System.out.println();
+		System.out.println("Name : " + name);
+		System.out.println("Technology : "+ technology);
+		System.out.println("Database : "+database);
+		System.out.println("No of Technology " + noofDevelopers);
+	}
+
+}
+
+class WebAppDriver
+{
+	public static void main(String[] args) {
+		WebApp obj = new WebApp("Berojgar.com","React.js","MongoDB",4);
+		obj.displayWebAppInfo();
+	}
 }
