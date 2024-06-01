535cd559d1bb73a98da6ce8da61192484c4a5516
Akash Morwal <akashbhau101@gmail.com>
2024-06-01 08:58:46 +0530

    new program is updated (01-06-2024)

---
+++ Pattern Program/Pattern39.java
@@ -0,0 +1,16 @@
+class Pattern39
+{
+	public static void main(String[] args) {
+		
+		int n = 5;
+
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
