e2591300ce4c41ce8f67505f31052b081b6a43d5
Akash Morwal <akashbhau101@gmail.com>
2024-06-02 16:55:58 +0530

    new program is updated (02-06-2024)

--- Pattern Program/Pattern41.java
+++ Pattern Program/Pattern41.java
@@ -4,14 +4,15 @@ class Pattern41
 {
 	public static void main(String[] args) {
 		
-		int n= 5;
+		int i,j,n=1;
 
-		for(int i=1;i<=n;i++)
+		for(int i=1;i<=4;i++)
 		{
 			for (int j=1;j<=i;j++) {
-				System.out.print((j*2)+"");
+				System.out.print((n);
+					n++;
 			}
-			System.out.println();
+			System.out.println("\n");
 		}
 	}
 }
