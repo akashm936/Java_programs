a7419776557415daebd8fca723b4b265f70c668a
Akash Morwal <akashbhau101@gmail.com>
2024-06-03 19:13:06 +0530

    new program is updated (03-06-2024)

---
+++ Pattern Program/Pattern80.java
@@ -0,0 +1,19 @@
+class Pattern80
+{
+	public static void main(String[] args) {
+		
+		int n=10; //
+
+		int k;
+		for(int i=n;i>=1;i--)
+		{
+			k=i;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.printf("%2d",k);
+				k=k+(n-j);
+			}
+			System.out.println();
+		}
+	}
+}
