3407dd68dd7d79d5583c5089f9a8fec625a14b50
Akash Morwal <akashbhau101@gmail.com>
2024-06-04 20:15:11 +0530

    new program is updated (04-06-2024)

---
+++ Pattern Program/Pattern101.java
@@ -0,0 +1,16 @@
+class Pattern101
+{
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int x = 2;
+
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				if
+			}
+		}
+	}
+}
