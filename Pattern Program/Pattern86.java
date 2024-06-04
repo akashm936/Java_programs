65865fbe3f51184a5ea3991f3209be464b98d1c4
Akash Morwal <akashbhau101@gmail.com>
2024-06-04 19:52:46 +0530

    new program is updated (04-06-2024)

--- Pattern Program/Pattern86.java
+++ Pattern Program/Pattern86.java
@@ -2,5 +2,15 @@ class Pattern86
 {
    public static void main(String[] args) {
    		
+   		int n = 5;
+
+   		for(int i=n;i>=1;i--)
+   		{
+   			for(int j=0;j<i;j++)
+   			{
+   				System.out.print(((i+j) % 2)+ " ");
+   			}
+   			System.out.println();
+   		}
    	}	
 }
