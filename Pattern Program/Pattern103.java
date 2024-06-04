1623d956a9244dfaa7686fae4dc41e3f43c1bab6
Akash Morwal <akashbhau101@gmail.com>
2024-06-04 20:27:39 +0530

    new program is updated (04-06-2024)

---
+++ Pattern Program/Pattern103.java
@@ -0,0 +1,19 @@
+class Pattern103
+{
+	public static void main(String[] args) {
+		
+		int n=5;
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int s=n-1;s>=i;s--)
+			{
+				System.out.print(" ");
+			}
+			for (int j=1;j<=i;j++) {
+				System.out.printf("%3d",(i*j));
+			}
+			System.out.println();
+		}
+	}
+}
