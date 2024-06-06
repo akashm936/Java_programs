6a63df8de26afec54d9ab12ec5253a280124fd05
Akash Morwal <akashbhau101@gmail.com>
2024-06-06 19:23:35 +0530

    new program is updated (06-06-2024)

--- Pattern Program/Pattern129.java
+++ Pattern Program/Pattern129.java
@@ -1,17 +1,18 @@
-class PatternProg
+
+class Pattern129
 {
 	public static void main(String[] args) {
 		
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=10;i++)
 		{
-			for(int k=4;k>=i;k--)
+			for(int k=9;k>=i;k--)
 			{
 				System.out.print(" ");
 			}
 			for (int j=1;j<=i;j++) {
-				System.out.print(i+ " ");
-				
+				System.out.print(i+ " ");	
 			}
+			System.out.print("\n");
 		}
 	}
 }
