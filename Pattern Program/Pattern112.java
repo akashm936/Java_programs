58554efdef3d7e7f98d15bdf38dc3e36bd8e7903
Akash Morwal <akashbhau101@gmail.com>
2024-06-07 22:21:11 +0530

    new program is updated (07-06-2024)

--- Pattern Program/Pattern112.java
+++ Pattern Program/Pattern112.java
@@ -2,6 +2,19 @@ class Pattern112
 {
 	public static void main(String[] args) 
 	{
-		
+		int n = 5; 
+
+		for(int i=0; i<n;i++)
+		{
+			for(int j= n-1;j>i;j--)
+			{
+				System.out.println(" ");
+			}
+			for (int k=0;k<=i;k++)
+			{
+				System.out.print((char)(k+65));
+			}
+			System.out.println();
+		}
 	}
 }
