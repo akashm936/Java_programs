89eae991a56e65443a9080582558bf9d5f3e038b
Akash Morwal <akashbhau101@gmail.com>
2024-06-07 22:36:03 +0530

    new program is updated ( 07-06-2024)

--- Pattern Program/Pattern143.java
+++ Pattern Program/Pattern143.java
@@ -2,6 +2,22 @@ class Pattern143
 {
 	public static void main(String[] args) {
 		
-		int
+		int n=5;
+		int z=1;
+
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=n-1;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=z;k>=1;k--)
+			{
+				System.out.print(k);
+			}
+			z+=2;
+			System.out.println();
+		}
 	}
 }
