f982f3adb73024d3d1aba9168aaf48bc66f3f9ef
Akash Morwal <akashbhau101@gmail.com>
2024-06-08 09:07:09 +0530

    new program of arrays are updated (08-06-2024)

---
+++ array/Array1.java
@@ -0,0 +1,27 @@
+class Array1
+{
+	public static void main(String[] args) {
+		
+		int [] a = {10,20,30,40};
+		
+       //for loop
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+		//while loop
+		int i= 0;
+		while(i<=a.length-1)
+		{
+			System.out.println(a[i++]);
+		} 
+        
+		//do while loop
+		do{
+			System.out.println(a[i++]);
+		}
+		while(i<=a.length);
+
+	}
+}
---
+++ array/Array2.java
@@ -0,0 +1,12 @@
+class Array2
+{
+	public static void main(String[] args) {
+		
+		int [] arr = {10,20,30,40,50,60};
+
+		for(int i:arr)
+		{
+			System.out.print(i + " ");
+		}
+	}
+}
