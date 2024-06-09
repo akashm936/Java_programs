b4c8572813b7ac2f9c5208fcf2b66e72303d2e8a
Akash Morwal <akashbhau101@gmail.com>
2024-06-09 23:22:03 +0530

    new program is updated (9-06-2024)

---
+++ array/Array1D.java
@@ -0,0 +1,18 @@
+class Array1D
+{
+	public static void main(String[] args) {
+		
+		int [] arr = {10,20,30,40,50,60,45};
+
+		// for(int i=0;i<arr.length;i++)
+		// {
+		// 	System.out.println(arr[i]);
+		// }
+
+        int i=0;
+		do{
+			System.out.println(arr[i]);
+		}
+		while(i<arr.length);
+	}
+}
