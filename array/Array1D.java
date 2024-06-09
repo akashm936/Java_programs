73dcfa0965e708767a6ced912567d52aee8caf71
Akash Morwal <akashbhau101@gmail.com>
2024-06-09 23:34:33 +0530

    new program is updated (09-06-2024)

--- array/Array1D.java
+++ array/Array1D.java
@@ -5,12 +5,14 @@ class Array1D
 	public static void main(String[] args) {
 		
 		int [] arr = {10,20,30,40,50,60,45};
+		char [] name = {'A','K','A','S','H'};
 
 //for loop
-		// for(int i=0;i<arr.length;i++)
-		// {
-		// 	System.out.println(arr[i]);
-		// }
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+			System.out.print(arr[i]+" ");
+		}
 
 //do while loop
         // int i=0;
