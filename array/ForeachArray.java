582fd40842b40ca9c8559210abf4ded82525e078
Akash Morwal <akashbhau101@gmail.com>
2024-06-10 10:01:38 +0530

    new program is updated (10-06-2024)

---
+++ array/ForeachArray.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class ForeachArray
+{
+	public static void main(String[] args) {
+		
+		int [] arr = {1,2,3,4,5,6,7,8};
+
+		int count=0;
+		for (int i : arr) {
+			if(i%2==0)
+				count++;
+			else
+				count++;
+		}
+
+		int [] evenNumbers = new int[count];
+		int [] oddNumbers  = new int[count];
+		int indx = 0;
+		for (int i :arr)
+		{
+			if(i%2==0)
+				evenNumbers[indx++]=i;
+			else
+			oddNumbers[indx++]=i;
+		}
+		System.out.println(Arrays.toString(evenNumbers));
+		System.out.println(Arrays.toString(oddNumbers));
+        
+
+
+        //---------------------------------------------------------
+        // int count=0;
+        // for(int i :arr)
+        // {
+        	
+        // }
+
+
+
+	}
+}
