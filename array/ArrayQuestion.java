6659aeee2064ce98e2d82fb6e4d957c77ad5a76e
Akash Morwal <akashbhau101@gmail.com>
2024-06-11 14:27:44 +0530

    new program is updated (11-06-2024)

---
+++ array/ArrayQuestion.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+class ArrayQuestion
+{
+	int triangleToy[4];
+	 
+}
---
+++ array/StringtoArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class StringtoArray
+{
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a String :");
+		String sen = sc.nextLine();
+
+		char [] arr = stringtoArray(sen);
+
+		String word ="";
+		for(int i=0;i<sen.length();i++)
+		{
+			char ch = sen.charAt(i);
+			if(ch==' '){
+				System.out.println(i);
+				System.out.println();
+				break;
+			}
+			word += ch;
+		}
+		System.out.println(word);
+	}
+}
