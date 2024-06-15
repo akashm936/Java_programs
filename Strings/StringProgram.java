d9f4330a4cdcf5f64ec5381748cfc5249dd046a5
Akash Morwal <akashbhau101@gmail.com>
2024-06-15 12:34:42 +0530

    new program is updated (15-06-2024)

---
+++ Strings/StringProgram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class StringProgram
+{
+	public static void main(String[] args) {
+		//find distinct characters from a String
+		String str = "abhahfjancb";
+		System.out.println(str);
+		int [] arr = new int[str.length()];
+		for(int i =0;i<str.length();i++)
+		{
+			
+		}
+	}
+}
