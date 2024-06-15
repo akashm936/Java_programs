381726f5d994dace0676c89052471331f7581f6f
Akash Morwal <akashbhau101@gmail.com>
2024-06-15 13:18:13 +0530

    new program is updated (15-06-2024)

--- array/SortAceOrderArrays.java
+++ array/SortAceOrderArrays.java
@@ -5,13 +5,53 @@ class SortAceOrderArrays
 {
 	public static void main(String[] args) {
 		
-		int []arr = {9,6,3,8,2};
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size : ");
+		int size = sc.nextInt();
 
-		for(int i=0;i<arr.length;i++);
+		System.out.println("Enter the element : ");
+		int []arr = new int[size];
+
+		for(int i=0;i<size;i++)
 		{
-			
+			arr[i]=sc.nextInt();
 		}
+		// comment
+		//---------------------------------------
+         // int min=arr[0];
 
+         // for(int j=0;j<arr.length;j++)
+         // {
+         // 	if(arr[j]!= min)
+         // 	{
+         // 		for(int i=0;i<arr.length;i++)
+         // 		{
+         // 			if(arr[i]<min)
+         // 			{
+         // 				min=arr[i];
+         // 			}
+         // 		}
+         // 		System.out.println("Ascending Order :" + min );
+         // 	}
+         }
+         
+		//----------------------------------------
+
+        //this is tracing of program
+		// int temp=0;
+		// for(int j=0;j<size;j++)
+		// {
+		// 	for(int i=0;i<size;i++)
+		// 	{
+		// 		if(arr[i]>arr[j])
+		// 		{
+		// 			temp = arr[i];
+		// 			arr[i]=arr[j];
+		// 			arr[j]=temp;
+		// 		}
+		// 	}
+		// 	System.out.println(Arrays.toString(arr));
+		// }
 	}
 }
 
