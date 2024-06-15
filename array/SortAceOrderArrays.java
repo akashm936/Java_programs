c5e8f7f8108581f3db6a4077f35d4f703b646f40
Akash Morwal <akashbhau101@gmail.com>
2024-06-15 13:21:03 +0530

    program is updated (15-06-2024)

--- array/SortAceOrderArrays.java
+++ array/SortAceOrderArrays.java
@@ -18,40 +18,24 @@ class SortAceOrderArrays
 		}
 		// comment
 		//---------------------------------------
-         // int min=arr[0];
-
-         // for(int j=0;j<arr.length;j++)
-         // {
-         // 	if(arr[j]!= min)
-         // 	{
-         // 		for(int i=0;i<arr.length;i++)
-         // 		{
-         // 			if(arr[i]<min)
-         // 			{
-         // 				min=arr[i];
-         // 			}
-         // 		}
-         // 		System.out.println("Ascending Order :" + min );
-         // 	}
-         }
+           
          
 		//----------------------------------------
 
-        //this is tracing of program
-		// int temp=0;
-		// for(int j=0;j<size;j++)
-		// {
-		// 	for(int i=0;i<size;i++)
-		// 	{
-		// 		if(arr[i]>arr[j])
-		// 		{
-		// 			temp = arr[i];
-		// 			arr[i]=arr[j];
-		// 			arr[j]=temp;
-		// 		}
-		// 	}
-		// 	System.out.println(Arrays.toString(arr));
-		// }
+		int temp=0;
+		for(int j=0;j<size;j++)
+		{
+			for(int i=0;i<size;i++)
+			{
+				if(arr[i]>arr[j])
+				{
+					temp = arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+		}
 	}
 }
 
