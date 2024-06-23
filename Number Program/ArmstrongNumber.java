f649c6101eab925f078429333275650ee8826d5e
Akash Morwal <akashbhau101@gmail.com>
2024-06-23 20:15:55 +0530

    new program is updated (23-06-2024)

--- Number Program/ArmstrongNumber.java
+++ Number Program/ArmstrongNumber.java
@@ -1,1 +1,31 @@
-ArmstrongNumber
+
+class ArmstrongNumber
+{
+	public static void main(String []args)
+	{
+		int num =1634;
+		int length =0;
+		int dup =num;
+		int dup1= num;
+		int sum =0;
+
+		while(num>0)
+		{
+			length++;
+			num/=10;
+		}
+
+		while(dup>0)
+		{
+			int rem=dup%10;
+			int power=1;
+			for(int i=1;i<=length;i++)
+			{
+				power*=rem;
+			}
+			sum+=power;
+			dup/=10;   
+		}
+		System.out.println((sum==dup1)?"ArmstrongNumber":"Not ArmstrongNumber");
+	}
+}
