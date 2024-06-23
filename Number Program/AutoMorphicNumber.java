7c2e04d45afce26221eeea4d6d0d1ab3deaa0767
Akash Morwal <akashbhau101@gmail.com>
2024-06-23 23:08:16 +0530

    new program is updated (23-06-2024)

--- Number Program/AutoMorphicNumber.java
+++ Number Program/AutoMorphicNumber.java
@@ -1,1 +1,19 @@
 class AutoMorphicNumber
+{
+	public static void main(String[] args) {
+		
+		int num= 76;
+		int dup=num;
+		int sqr=num*num;
+		int div =1;
+
+		while(num!=0)
+		{
+			div*=10;
+			num/=10;
+		}
+		int id=sqr%div;
+
+		System.out.println((dup==id)?"AutoMorphicNumber":"Not a AutoMorphicNumber");
+	}
+}
