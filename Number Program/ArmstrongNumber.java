0fbb7b6c79feb141a557bac0011132253fd4c4cc
Akash Morwal <akashbhau101@gmail.com>
2024-06-23 20:19:06 +0530

    new program is modified (23-06-2024)

--- Number Program/ArmstrongNumber.java
+++ Number Program/ArmstrongNumber.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 class ArmstrongNumber
 {
 	public static void main(String []args)
 	{
-		int num =1634;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a Number : ");
+		int num = sc.nextInt();
+		//int num =1634;
 		int length =0;
 		int dup =num;
 		int dup1= num;
