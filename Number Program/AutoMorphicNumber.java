8b88cbea8d3ef450c792b402181d408b408872bd
Akash Morwal <akashbhau101@gmail.com>
2024-06-23 23:11:34 +0530

    new program is updated (23-06-2024)

--- Number Program/AutoMorphicNumber.java
+++ Number Program/AutoMorphicNumber.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 class AutoMorphicNumber
 {
 	public static void main(String[] args) {
 		
-		int num= 76;
+		Scanner sc = new Scanner(System.in);
+        
+        System.out.println(" To check AutoMorphicNumber ");
+		System.out.println("Enter a Numebr : ");
+        int num = sc.nextInt();
+
+		//int num= 76;
 		int dup=num;
 		int sqr=num*num;
 		int div =1;
